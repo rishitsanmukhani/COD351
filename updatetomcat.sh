5c89479ccc8d1c7b034e0cc75132c70dcc75fbc7
Rishit_India <rishitsanmukhani@gmail.com>
2017-04-14 23:33:59 +0530

    tomcat server configuration done, some bug fixes required

---
+++ updatetomcat.sh
@@ -0,0 +1,1 @@
+sudo rm -R /var/lib/tomcat7/webapps/ROOT/WEB-INF/classes/* ; sudo cp -R /home/rishit/workspace/COD351/bin/* /var/lib/tomcat7/webapps/ROOT/WEB-INF/classes/; sudo service tomcat7 restart
